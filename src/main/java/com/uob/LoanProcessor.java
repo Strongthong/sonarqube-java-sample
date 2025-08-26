07de37467323291c5c9502f018c712b44a26a2b6
Strongthong <89025122+Strongthong@users.noreply.github.com>
2025-08-26 16:04:56 +0800

    Update LoanProcessor.java

--- src/main/java/com/uob/LoanProcessor.java
+++ src/main/java/com/uob/LoanProcessor.java
@@ -3,6 +3,7 @@ package com.uob;
 public class LoanProcessor {
     public void processLoan() {
         // TODO: add proper validation (Sonar will flag TODOs)
+        TODO: add proper validation 
         System.out.println("Processing loan.x.");
     }
 }
