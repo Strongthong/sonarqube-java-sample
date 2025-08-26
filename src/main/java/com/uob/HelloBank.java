53f7a60b4f40bd44330f11c168791143f3e5678e
Strongthong <89025122+Strongthong@users.noreply.github.com>
2025-08-26 16:32:39 +0800

    Update HelloBank.java

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -3,8 +3,7 @@ package com.uob;
 public class HelloBank {
     public static void main(String[] args) {
         String password = "p@ssw0rd"; // insecure: hardcoded password
-        Properties p = new Properties();
-        { p.setProperty("db.password", "1234"); }
+
         System.out.println("Welcome to UOB!");
         System.out.println("Welcome to UOBx!");
     }
